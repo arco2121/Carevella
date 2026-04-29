ef11a7e00c7e203be1e19a48c286cba18c4bab7d
Marco Colombara <colombaramarco21@gmail.com>
2026-04-29 14:02:34 +0200

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="row full-width vertical_center orizionat_center padding_vertical_20">
-    <h6><strong>IOT-Project-Laravel Copyrightt© 2026 - Versione {{ $version  }}</strong></h6>
+    <h6><strong>IOT-Project-Laravel Copyright© 2026 - Versione {{ $version  }}</strong></h6>
 </footer>
 
 @vite(['resources/js/components/clearDom.js'])
